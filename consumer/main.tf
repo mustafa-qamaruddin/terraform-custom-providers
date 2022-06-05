fb6bb8763ae028dfe725de3b1518c4e2e3d77688
Mustafa Mahrous <mustafa.mahrous@qaware.de>
2022-06-05 13:58:37 +0200

    add read client/data source

--- consumer/main.tf
+++ consumer/main.tf
@@ -12,21 +12,36 @@ provider "hashicups" {
     password = "test123"
 }
 
-resource "hashicups_order" "edu" {
+resource "hashicups_order" "edu2" {
     items {
         coffee {
             id = 3
         }
-        quantity = 2
+        quantity = 3
     }
     items {
         coffee {
             id = 3
         }
-        quantity = 2
+        quantity = 3
+    }
+    items {
+        coffee {
+            id = 3
+        }
+        quantity = 4
     }
 }
 
+
+data "hashicups_ingredients" "first_coffee" {
+    coffee_id = hashicups_order.edu2.items[0].coffee[0].id
+}
+
 output "edu_order" {
-    value = "hashicups_order.edu"
+    value = resource.hashicups_order.edu2
+}
+
+output "first_coffee_ingredients" {
+    value = data.hashicups_ingredients.first_coffee
 }
